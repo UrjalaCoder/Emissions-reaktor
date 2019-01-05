8314cc2c8e95741b0b8500da6eb3e8ea47821bef
Tuomas Myllymaki <myllymakituomas1998@gmail.com>
2019-01-05 18:57:32 +0200

    Styling

--- static/js/scriptUI2.js
+++ static/js/scriptUI2.js
@@ -115,6 +115,9 @@ function createGraph() {
                         beginAtZero: true
                     }
                 }]
+            },
+            legend: {
+                display: false
             }
         }
     });
